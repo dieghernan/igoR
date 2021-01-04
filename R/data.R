5673cf86dbe8b87ec7c7e22cb61e201ceb064ec9
dieghernan <diego.hernangomezherrero@gmail.com>
2021-01-04 02:41:47 +0100

    Add data

---
+++ R/data.R
@@ -0,0 +1,25 @@
+#' @title Intergovernmental Organizations (IGO) by year
+#' @name igo_year_format3
+#' @docType data
+#' @description Data on IGOs from 1815-2014, at the IGO-year level.
+#' Contains one record per IGO-year (with years listed at 5 year intervals
+#' through 1965, and annually thereafter).
+#' @source \href{https://correlatesofwar.org/data-sets/IGOs}{Intergovernmental Organizations (v3)},
+#' The Correlates of War Project (IGO Data Stata Files)
+#' @format data frame with 19335 rows. See \strong{Codebook Version 3 IGO Data} for full reference.
+#' @seealso \code{\link{state_year_format3}}
+#' @note Raw data used internally by \pkg{igoR}
+NULL
+
+#' @title Country membership to IGO by year
+#' @name state_year_format3
+#' @docType data
+#' @description Data on IGOs from 1815-2014, at the country-year level.
+#' Contains one record per country-year (with years listed at 5 year
+#' intervals through 1965, and annually thereafter).
+#' @source \href{https://correlatesofwar.org/data-sets/IGOs}{Intergovernmental Organizations (v3)},
+#' The Correlates of War Project (IGO Data Stata Files)
+#' @format data frame with 15557 rows. See \strong{Codebook Version 3 IGO Data} for full reference.
+#' @seealso \code{\link{igo_year_format3}}
+#' @note Raw data used internally by \pkg{igoR}
+NULL
---
+++ data-raw/getdata.R
@@ -0,0 +1,32 @@
+# Download
+rm(list = ls() )
+
+library(dplyr)
+
+curl::curl_download(
+  "https://correlatesofwar.org/data-sets/IGOs/codebook-version-3-igo-data/@@download/file/IGO%20Codebook_v3_short%20copy.pdf",
+  "./data-raw/IGO Codebook_v3_short copy.pdf"
+)
+
+# Files
+
+# download.file(
+#   "https://correlatesofwar.org/data-sets/IGOs/igo-data-stata-files-zip/@@download/file/IGO_stata.ZIP",
+#   "./data-raw/IGO_stata.ZIP"
+# )
+
+
+# unzip("./data-raw/igo_year_formatv3.zip", exdir = "./data-raw/igo_year_formatv3")
+
+# Read
+
+igo_year_format3 <- haven::read_dta("data-raw/igo_year_format_3.dta") %>% as.data.frame()
+
+state_year_format3 <-
+  haven::read_dta("./data-raw/state_year_format3.dta") %>% as.data.frame()
+
+
+usethis::use_data(igo_year_format3, overwrite = TRUE)
+usethis::use_data(state_year_format3, overwrite = TRUE)
+
+tools::checkRdaFiles("./data")
