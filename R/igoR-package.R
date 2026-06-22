6a5d8e4ec2f6dc442feadfcfb64c11d3ed23ac31
Diego H. <dieghernan@users.noreply.github.com>
2026-06-23 00:10:09 +0200

    improve references

--- R/igoR-package.R
+++ R/igoR-package.R
@@ -2,7 +2,7 @@
 #' @references
 #' Pevehouse, J. C., Nordstrom, T., McManus, R. W. & Jamison, A. S. (2020).
 #' Tracking organizations in the world: The Correlates of War IGO Version 3.0
-#' data sets. *Journal of Peace Research, 57*(3), 492–503.
+#' data sets. *Journal of Peace Research*, **57**(3), 492–503.
 #' \doi{10.1177/0022343319881175}.
 #' @encoding UTF-8
 "_PACKAGE"
